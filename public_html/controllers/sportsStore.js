ab90a51e69c3236de89de068e2278ac7fd64775c
glpaparelli <giuliopaparelli@gmail.com>
2015-07-17 19:02:53 +0200

    with pain, finished chapter 8

--- public_html/controllers/sportsStore.js
+++ public_html/controllers/sportsStore.js
@@ -8,7 +8,9 @@
 
 angular.module("sportsStore")
 .constant("dataUrl", "http://localhost:5500/products")
-.controller("sportsStoreCtrl", function ($scope, $http, dataUrl){
+.constant("orderUrl","http://localhost:5500/orders")
+.controller("sportsStoreCtrl", function ($scope, $http, $location,
+     dataUrl, orderUrl, cart){
     
         $scope.data = {};
         
@@ -19,4 +21,27 @@ angular.module("sportsStore")
         .error(function (error){
             $scope.data.error = error;
         });
+        
+        $scope.sendOrder = function (shippingDetails) {
+            var order = angular.copy(shippingDetails);
+            order.products = cart.getProducts();
+            $http.post(orderUrl, order)
+            .success(function (data) {
+                $scope.data.orderId = data.id;
+                cart.getProducts().length = 0;
+            }).error(function (error) {
+                $scope.data.orderError = error;
+            }).finally(function () {
+                $location.path("/complete");
+            });
+        };
+    
 });
+
+
+
+
+
+
+
+
